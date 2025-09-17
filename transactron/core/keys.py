4f99bec71c28e2d4355c6628e13fce11ec15dd01
Marek Materzok <tilk@tilk.eu>
2025-09-17 10:59:54 +0200

    Collect transactions and methods using keys (#99)

--- transactron/core/keys.py
+++ transactron/core/keys.py
@@ -5,12 +5,23 @@ from dataclasses import dataclass
 if TYPE_CHECKING:
     from .manager import TransactionManager  # noqa: F401 because of https://github.com/PyCQA/pyflakes/issues/571
     from .method import Method  # noqa: F401 because of https://github.com/PyCQA/pyflakes/issues/571
+    from .transaction import Transaction  # noqa: F401 because of https://github.com/PyCQA/pyflakes/issues/571
 
-__all__ = ["TransactionManagerKey", "ProvidedMethodsKey"]
+__all__ = ["TransactionManagerKey", "TransactionsKey", "DefinedMethodsKey", "ProvidedMethodsKey"]
 
 
 @dataclass(frozen=True)
 class TransactionManagerKey(SimpleKey["TransactionManager"]):
+    pass
+
+
+@dataclass(frozen=True)
+class TransactionsKey(ListKey["Transaction"]):
+    pass
+
+
+@dataclass(frozen=True)
+class DefinedMethodsKey(ListKey["Method"]):
     pass
 
 
