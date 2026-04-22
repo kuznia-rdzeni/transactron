fc16367dbf4659651beff5823df23ee022a94fb4
Janeczko Jakub <kuba@ev1.pl>
2026-04-22 22:37:24 +0200

    Probably actually fix method readiness (maybe) (#146)

--- transactron/core/manager.py
+++ transactron/core/manager.py
@@ -99,6 +99,10 @@ class MethodMap:
     def methods_and_transactions(self) -> Iterable[Body]:
         return chain(self.methods, self.transactions)
 
+    def ready_for_transaction(self, trans: TBody) -> Collection[Body]:
+        # all bodies that need to be ready for transaction to run
+        return [trans] + self.methods_by_transaction[trans]
+
 
 class TransactionManager(Elaboratable):
     """Transaction manager
@@ -121,9 +125,13 @@ class TransactionManager(Elaboratable):
         ]
 
     @staticmethod
+    def _transaction_and_called_methods(method_map: MethodMap, trans: TBody):
+        return [trans] + method_map.methods_by_transaction[trans]
+
+    @staticmethod
     def _transactions_exclusive(method_map: MethodMap, trans1: TBody, trans2: TBody):
-        tms1 = [trans1] + method_map.methods_by_transaction[trans1]
-        tms2 = [trans2] + method_map.methods_by_transaction[trans2]
+        tms1 = method_map.ready_for_transaction(trans1)
+        tms2 = method_map.ready_for_transaction(trans2)
 
         # if first transaction is exclusive with the second transaction, or this is true for
         # any called methods, the transactions will never run at the same time
@@ -245,21 +253,16 @@ class TransactionManager(Elaboratable):
         return method_enables
 
     @staticmethod
-    def _ready_dependencies(method_map: MethodMap) -> TransactionGraph:
-        ready_dependencies = defaultdict[TBody, set[TBody]](set)
+    def _ready_dependencies(transactions: Sequence[Transaction], methods: Sequence[Method]) -> Graph[Body]:
+        ready_dependencies = defaultdict[Body, set[Body]](set)
 
-        relations = TransactionManager._relations(method_map)
+        for elem in chain(transactions, methods):
+            body = elem._body
+            for relation in body.relations:
+                if not relation.ready_dependent:
+                    continue
 
-        for relation in relations:
-            if not relation.ready_dependent:
-                continue
-
-            for trans_start in method_map.transactions_for(relation.start):
-                for trans_end in method_map.transactions_for(relation.end):
-                    if trans_start is not trans_end and not TransactionManager._transactions_exclusive(
-                        method_map, trans_start, trans_end
-                    ):
-                        ready_dependencies[trans_end].add(trans_start)
+                ready_dependencies[relation.end].add(body)
 
         return ready_dependencies
 
@@ -416,7 +419,7 @@ class TransactionManager(Elaboratable):
             method_map = MethodMap(self.transactions)
             cgr, porder = TransactionManager._conflict_graph(method_map)
 
-        ready_dependencies = TransactionManager._ready_dependencies(method_map)
+        ready_dependencies = TransactionManager._ready_dependencies(self.transactions, self.methods)
 
         for transaction in method_map.transactions:
             for dep in ready_dependencies[transaction]:
@@ -452,7 +455,9 @@ class TransactionManager(Elaboratable):
                 method._validate_arguments(method_map.argument_by_call[transaction, method])
                 for method in method_map.methods_by_transaction[transaction]
             ]
-            runnable_terms.extend(dep.ready for dep in ready_dependencies[transaction])
+            runnable_terms.extend(
+                dep.run for body in method_map.ready_for_transaction(transaction) for dep in ready_dependencies[body]
+            )
             m.d.comb += transaction.runnable.eq(Cat(runnable_terms).all())
 
         ccs = _graph_ccs(cgr)
