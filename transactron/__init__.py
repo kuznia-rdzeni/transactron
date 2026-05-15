91461bf84d398f20f5973ee8d4450cac8cba2e48
Marek Materzok <tilk@tilk.eu>
2026-05-15 12:46:13 +0200

    Changed transactron.__all__

--- transactron/__init__.py
+++ transactron/__init__.py
@@ -1,1 +1,34 @@
-from .core import *  # noqa: F401
+from .core import (
+    TModule,
+    Method,
+    Methods,
+    Transaction,
+    def_method,
+    def_methods,
+    Required,
+    Provided,
+    TransactronContextElaboratable,
+    TransactronContextComponent,
+)
+from .utils import assign, AssignType, assertion
+from .lib import condition
+
+__all__ = [
+    # core
+    "TModule",
+    "Method",
+    "Methods",
+    "Transaction",
+    "def_method",
+    "def_methods",
+    "Required",
+    "Provided",
+    "TransactronContextElaboratable",
+    "TransactronContextComponent",
+    # utils
+    "assign",
+    "AssignType",
+    "assertion",
+    # lib
+    "condition",
+]
